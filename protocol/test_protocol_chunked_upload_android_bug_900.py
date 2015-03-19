9fd94db7f851fde730d96cd0f97cfd5274ab8d63
Jakub Moscicki <Jakub.Moscicki@cern.ch>
2015-03-19 12:21:09 +0100

    renamed MB to ANDROID_CHUNKSIZE

--- protocol/test_protocol_chunked_upload_android_bug_900.py
+++ protocol/test_protocol_chunked_upload_android_bug_900.py
@@ -10,17 +10,20 @@ def main(step):
 
     URL = oc_webdav_url()
 
-    MB=1000*1000
+    # chunk size defined in the android source code
+    # https://github.com/owncloud/android-library/blob/d7097983594347167b5bde3fa5b2b4ad1d843392/src/com/owncloud/android/lib/resources/files/ChunkedUploadRemoteFileOperation.java#L45
 
-    filename=create_hashfile(d,size=int(5.5*MB))
+    ANDROID_CHUNKSIZE=1024*1000 
+
+    filename=create_hashfile(d,size=int(5.5*ANDROID_CHUNKSIZE))
 
     # careful with the chunk size...
-    r=chunk_file_upload(filename,URL,chunk_size=1*MB,android_client_bug_900=True)
+    r=chunk_file_upload(filename,URL,chunk_size=ANDROID_CHUNKSIZE,android_client_bug_900=True)
     file_download(os.path.basename(filename),URL,d)
     analyse_hashfiles(d)
 
     # upload again matching the existing etag
-    r=chunk_file_upload(filename,URL,chunk_size=1*MB,android_client_bug_900=True,header_if_match=r.headers['ETag'])
+    r=chunk_file_upload(filename,URL,chunk_size=ANDROID_CHUNKSIZE,android_client_bug_900=True,header_if_match=r.headers['ETag'])
     analyse_hashfiles(d)
 
     # upload again with a non-matching etag
