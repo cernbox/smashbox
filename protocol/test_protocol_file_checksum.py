2e10b5bb9664dfba9f25ea5b9303997f6bbea477
Jakub Moscicki <Jakub.Moscicki@cern.ch>
2015-06-12 18:42:53 +0200

    added more verbose logging and paranoia check for uploads with incorrect checksum

--- protocol/test_protocol_file_checksum.py
+++ protocol/test_protocol_file_checksum.py
@@ -1,10 +1,10 @@
 from smashbox.utilities import *
 from smashbox.utilities.hash_files import *
 from smashbox.protocol import chunk_file_upload, file_upload, file_download
-
 import smashbox.protocol
 
-
+import os
+import os.path
 
 @add_worker
 def main(step):
@@ -13,6 +13,8 @@ def main(step):
     reset_owncloud_account()
 
     URL = oc_webdav_url()
+
+    logger.info('*** 0. upload without the checksum (files should be accepted by the server)')
 
     filename1=create_hashfile(d,size=OWNCLOUD_CHUNK_SIZE(0.1))
     filename2=create_hashfile(d,size=OWNCLOUD_CHUNK_SIZE(3.3))
@@ -38,7 +40,8 @@ def main(step):
     logger.info('Server supports %s checksum',repr(active_server_checksum_type))
     smashbox.protocol.enable_checksum(active_server_checksum_type)
 
-    # pass around correct checksum
+    logger.info('*** 1. upload with correct checksum (files should be accepted by the server)')
+
     filename1=create_hashfile(d,size=OWNCLOUD_CHUNK_SIZE(0.1))
     filename2=create_hashfile(d,size=OWNCLOUD_CHUNK_SIZE(3.3))
 
@@ -58,17 +61,32 @@ def main(step):
         c = c[:-1]+chr(ord(c[-1])+1)
         return c
 
+    logger.info('*** 2. upload with corrupted checksum value (files should be refused by the server)')
+
+    filename1=create_hashfile(d,size=OWNCLOUD_CHUNK_SIZE(0.1))
+    filename2=create_hashfile(d,size=OWNCLOUD_CHUNK_SIZE(3.3))        
+
     r = file_upload(filename1,URL,checksum=corrupted_checksum(filename1))
     fatal_check(r.rc == 412)
 
     r = chunk_file_upload(filename2,URL,checksum=corrupted_checksum(filename2))
     fatal_check(r.rc == 412)
 
+    # paranoia check: make sure that the server did not store the corrupted files inspite of returning 412
+
+    d_corrupted = mkdir(os.path.join(d,'corrupted_files'))
+    file_download(os.path.basename(filename1),URL,d_corrupted)
+    file_download(os.path.basename(filename2),URL,d_corrupted)
+
+    fatal_check(os.listdir(d_corrupted)==[])
+
     # pass around a checksum of the type unsupported by the server, including some garbage types (which are not even well-formatted)
     # in this case the checksums should be ignored and the files transmitted as if checksum was not provided at all
     checksum_types = list(set(smashbox.protocol.known_checksum_types)-set([active_server_checksum_type]))
 
     checksum_types += ['blabla']
+
+    logger.info('*** 3. upload with corrupted checksum types and strange values (files should be accepted by the server)')
 
     for value in ['',':bah',':']:
         for cstype in checksum_types:
@@ -78,6 +96,8 @@ def main(step):
             filename1=create_hashfile(d,size=OWNCLOUD_CHUNK_SIZE(0.1))
             filename2=create_hashfile(d,size=OWNCLOUD_CHUNK_SIZE(3.3))        
 
+            logger.info(' *** testing checkum value: '+cstype+value)
+            
             file_upload(filename1,URL,checksum=cstype+value)
             chunk_file_upload(filename2,URL,checksum=cstype+value)
 
