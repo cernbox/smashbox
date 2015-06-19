cernbox/smashbox

commit 71a582d4adff62b9d4a4e05d31a3eed619f6bc0c

added expect_webdav methods

python/smashbox/utilities/__init__.py
@@ -279,6 +279,16 @@ def run_ocsync(local_folder, remote_folder="", n=None, user_num=None):
 def webdav_propfind_ls(path):
     runcmd('curl -s -k %s -XPROPFIND %s | xmllint --format -'%(config.get('curl_opts',''),oc_webdav_url(remote_folder=path)))
 
+def expect_webdav_does_not_exist(path):
+    exitcode,stdout,stderr = runcmd('curl -s -k %s -XPROPFIND %s | xmllint --format - | grep NotFound | wc -l'%(config.get('curl_opts',''),oc_webdav_url(remote_folder=path)))
+    exists = stdout.rstrip() == "1"
+    error_check(exists, "Remote path %s exists but should not" % path)
+
+def expect_webdav_exist(path):
+    exitcode,stdout,stderr = runcmd('curl -s -k %s -XPROPFIND %s | xmllint --format - | grep NotFound | wc -l'%(config.get('curl_opts',''),oc_webdav_url(remote_folder=path)))
+    exists = stdout.rstrip() == "0"
+    error_check(exists, "Remote path %s exists but should not" % path)
+
 def webdav_delete(path):
     runcmd('curl -k %s -X DELETE %s '%(config.get('curl_opts',''),oc_webdav_url(remote_folder=path)))
 
@@ -312,7 +322,7 @@ def runcmd(cmd,ignore_exitcode=False,echo=True,allow_stderr=True,shell=True,log_
         if not ignore_exitcode:
             raise subprocess.CalledProcessError(process.returncode,cmd)
 
-    return process.returncode
+    return (process.returncode, stdout, stderr)
 
 
 def sleep(n):

commit 024d8a614366f66cb825baa7f79d86ef2646d418

Correctly grab return code from runcmd
Broken since 71a582d4adff62b9d4a4e05d31a3eed619f6bc0c

python/smashbox/utilities/__init__.py
@@ -508,30 +508,30 @@ def scrape_log_file(d):
 
     """
     cmd = 'scp -P %d root@%s:%s/owncloud.log %s/.' % (config.scp_port, config.oc_server, config.oc_server_datadirectory, d)
-    rtn_code = runcmd(cmd)
+    rtn_code,stdout,stderr = runcmd(cmd)
 
     logger.info('copy command returned %s', rtn_code)
 
     # search logfile for string (1 == not found; 0 == found):
 
     cmd = "grep -i \"integrity constraint violation\" %s/owncloud.log" % d
-    rtn_code = runcmd(cmd, ignore_exitcode=True, log_warning=False)
+    rtn_code,stdout,stderr = runcmd(cmd, ignore_exitcode=True, log_warning=False)
     error_check(rtn_code > 0, "\"Integrity Constraint Violation\" message found in server log file")
 
     cmd = "grep -i \"Exception\" %s/owncloud.log" % d
-    rtn_code = runcmd(cmd, ignore_exitcode=True, log_warning=False)
+    rtn_code,stdout,stderr = runcmd(cmd, ignore_exitcode=True, log_warning=False)
     error_check(rtn_code > 0, "\"Exception\" message found in server log file")
 
     cmd = "grep -i \"could not obtain lock\" %s/owncloud.log" % d
-    rtn_code = runcmd(cmd, ignore_exitcode=True, log_warning=False)
+    rtn_code,stdout,stderr = runcmd(cmd, ignore_exitcode=True, log_warning=False)
     error_check(rtn_code > 0, "\"Could Not Obtain Lock\" message found in server log file")
 
     cmd = "grep -i \"db error\" %s/owncloud.log" % d
-    rtn_code = runcmd(cmd, ignore_exitcode=True, log_warning=False)
+    rtn_code,stdout,stderr = runcmd(cmd, ignore_exitcode=True, log_warning=False)
     error_check(rtn_code > 0, "\"DB Error\" message found in server log file")
 
     cmd = "grep -i \"stat failed\" %s/owncloud.log" % d
-    rtn_code = runcmd(cmd, ignore_exitcode=True, log_warning=False)
+    rtn_code,stdout,stderr = runcmd(cmd, ignore_exitcode=True, log_warning=False)
     error_check(rtn_code > 0, "\"Stat Failed\" message found in server log file")
 
 