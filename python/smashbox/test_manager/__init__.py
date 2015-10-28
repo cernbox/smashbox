5cd835846a0915a15998e2e943dafdfd4619a1c1
piotr <mrow4a@yahoo.com>
2015-10-28 10:28:48 +0100

    added test_manager, reporter class added to test_manager. Manager prepared for handling of non-native-sync-engines

---
+++ python/smashbox/test_manager/__init__.py
@@ -0,0 +1,32 @@
+
+class Test_Manager:
+    """ Manage all the test plugins and handle the test execution
+    """
+
+    def __init__(self,name,config):
+        self.LOG=True
+        self.name = name
+        self.config = config
+        from smashbox.utilities import curl_check_url
+        curl_check_url(config)
+        import smashbox.test_manager.reporter
+        self.reporter = smashbox.test_manager.reporter.Reporter(name,config)
+        
+    def setup_test(self):
+        if self.LOG:
+            print "TESTCASE_START",self.name,self.config.loop_i,self.config.testset_i,self.config.test_doc
+        self.reporter.reporter_setup_test()
+    
+    def finalize_test(self):
+        if self.LOG:
+            print "TESTCASE_STOP"
+        self.reporter.reporter_finalize_test()
+        
+    def setup_worker(self,manager,fname):
+        self.reporter.reporter_setup_worker(manager,fname)
+        
+    
+    def finalize_step(self,sync_exec_time_array, reported_errors,fname): 
+        self.reporter.reporter_finalize_step(sync_exec_time_array, reported_errors,fname)
+            
+
