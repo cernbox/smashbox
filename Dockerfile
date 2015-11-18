b10e263c15dd041000ab7053195ea595d5a60b74
root <mrow4a@yahoo.com>
2015-11-18 15:20:53 +0100

    ver 2 not stable

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,12 @@
 FROM ubuntu:14.04
+
 MAINTAINER Piotr Mrowczynski <piotr.mrowczynski@yahoo.com>
-RUN sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/xUbuntu_14.04/ /' >> /etc/apt/sources.list.d/owncloud-client.list"
+RUN apt-get install wget -y
+RUN sh -c "echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/xUbuntu_14.04/ /' >> /etc/apt/sources.li$
+RUN wget http://download.opensuse.org/repositories/isv:ownCloud:desktop/xUbuntu_14.04/Release.key
+RUN apt-key add - < Release.key
 RUN apt-get update
 RUN apt-get upgrade -y
 RUN apt-get install git-all -y
-RUN apt-get install owncloud-client -y
-FROM ubuntu:14.04
-
-# Replace 1000 with your user / group id
-RUN export uid=1000 gid=1000 && \
-    mkdir -p /home/developer && \
-    echo "developer:x:${uid}:${gid}:Developer,,,:/home/developer:/bin/bash" >> /etc/passwd && \
-    echo "developer:x:${uid}:" >> /etc/group && \
-    echo "developer ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/developer && \
-    chmod 0440 /etc/sudoers.d/developer && \
-    chown ${uid}:${gid} -R /home/developer
-
-USER developer
-ENV HOME /home/developer
+RUN sudo apt-get install owncloud-client -y
+RUN apt-get install python-netifaces -y
--- python/smashbox/test_manager/non_native_engine.py
+++ python/smashbox/test_manager/non_native_engine.py
@@ -149,7 +149,11 @@ def start_dropbox(fname, smashdir, get_running=True):
     process = subprocess.Popen(cmd, shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE,universal_newlines=True,)
     if(get_running==True):
         import time
+        nbsr = NonBlockingStreamReader(process.stdout)
         while True:
+            output = nbsr.readline(0.1)
+            if output:
+                print output
             stdout = check_status_dropbox(str(fname), smashdir)
             if((stdout.find("Up to date") != -1)):
                 break
@@ -424,3 +428,43 @@ def time_now(time_zero=None):
     else:
         return (datetime.datetime.now()-time_zero)  
   
+  
+from threading import Thread
+from Queue import Queue, Empty
+
+class NonBlockingStreamReader:
+
+    def __init__(self, stream):
+        '''
+        stream: the stream to read from.
+                Usually a process' stdout or stderr.
+        '''
+
+        self._s = stream
+        self._q = Queue()
+
+        def _populateQueue(stream, queue):
+            '''
+            Collect lines from 'stream' and put them in 'quque'.
+            '''
+
+            while True:
+                line = stream.readline()
+                if line:
+                    queue.put(line)
+                else:
+                    raise UnexpectedEndOfStream
+
+        self._t = Thread(target = _populateQueue,
+                args = (self._s, self._q))
+        self._t.daemon = True
+        self._t.start() #start collecting lines from the stream
+
+    def readline(self, timeout = None):
+        try:
+            return self._q.get(block = timeout is not None,
+                    timeout = timeout)
+        except Empty:
+            return None
+
+class UnexpectedEndOfStream(Exception): pass
