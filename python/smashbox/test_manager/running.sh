b73a5b745e5aef701e115575cc7312dd53e0ba32
root <mrow4a@yahoo.com>
2015-12-01 10:22:29 +0100

    update running.sh to support reboot option

--- python/smashbox/test_manager/running.sh
+++ python/smashbox/test_manager/running.sh
@@ -2,10 +2,17 @@
 PROCESS=$1
 PIDS=`ps cax | grep $PROCESS | grep -o '^[ ]*[0-9]*'`
 if [ -z "$PIDS" ]; then
-  val="Process not running"	
-  echo $val
+	if [ -z "$2" ]
+	then    
+  		val="Process not running"	
+  		echo $val
+	else
+		if [ "$2" = "reboot" ]; then
+			shutdown -r now 
+		fi
+	fi
 else
-  for PID in $PIDS; do
-    echo $PID
-  done
+  	for PID in $PIDS; do
+    	echo $PID
+  	done
 fi
