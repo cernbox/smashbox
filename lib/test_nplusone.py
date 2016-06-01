63bbb68743d18a3dd24609d197d44358d85babd9
Jakub Moscicki <Jakub.Moscicki@cern.ch>
2016-06-01 11:53:22 +0200

    merged changes from cboxsls: simple graphite monitoring, infinite loop support and fscheck in nplusone

--- lib/test_nplusone.py
+++ lib/test_nplusone.py
@@ -8,9 +8,13 @@ __doc__ = """ Add nfiles to a directory and check consistency.
 
 from smashbox.utilities import *
 from smashbox.utilities.hash_files import *
+from smashbox.utilities.monitoring import push_to_monitoring
 
 nfiles = int(config.get('nplusone_nfiles',10))
 filesize = config.get('nplusone_filesize',1000)
+
+# optional fs check before files are uploaded by worker0
+fscheck = config.get('nplusone_fscheck',False)
 
 if type(filesize) is type(''):
     filesize = eval(filesize)
@@ -54,8 +58,29 @@ def worker0(step):
 
     step(2,'Add %s files and check if we still have k1+nfiles after resync'%nfiles)
 
+    total_size=0
+    sizes=[]
+
+    # compute the file sizes in the set
     for i in range(nfiles):
-        create_hashfile(d,size=filesize)
+        size=size2nbytes(filesize)
+        sizes.append(size)
+        total_size+=size
+
+    time0=time.time()
+
+    logger.log(35,"Timestamp %f Files %d TotalSize %d",time.time(),nfiles,total_size)
+
+    # create the test files
+    for size in sizes:
+        create_hashfile(d,size=size)
+
+    if fscheck:
+        # drop the caches (must be running as root on Linux)
+        runcmd('echo 3 > /proc/sys/vm/drop_caches')
+        
+        ncorrupt = analyse_hashfiles(d)[2]
+        fatal_check(ncorrupt==0, 'Corrupted files ON THE FILESYSTEM (%s) found'%ncorrupt)
 
     run_ocsync(d)
 
@@ -68,6 +93,17 @@ def worker0(step):
     fatal_check(ncorrupt==0, 'Corrupted files (%s) found'%ncorrupt)
 
     logger.info('SUCCESS: %d files found',k1)
+
+    step(4,"Final report")
+
+    time1 = time.time()
+    push_to_monitoring("cernbox.cboxsls.nplusone.nfiles",nfiles)
+    push_to_monitoring("cernbox.cboxsls.nplusone.total_size",total_size)
+    push_to_monitoring("cernbox.cboxsls.nplusone.elapsed",time1-time0)
+    push_to_monitoring("cernbox.cboxsls.nplusone.total_size",total_size)
+    push_to_monitoring("cernbox.cboxsls.nplusone.transfer_rate",total_size/(time1-time0))
+    push_to_monitoring("cernbox.cboxsls.nplusone.worker0.synced_files",k1-k0)
+
         
 @add_worker
 def worker1(step):
@@ -83,9 +119,13 @@ def worker1(step):
     ncorrupt = analyse_hashfiles(d)[2]
     k1 = count_files(d)
 
+    push_to_monitoring("cernbox.cboxsls.nplusone.worker1.synced_files",k1-k0)
+    push_to_monitoring("cernbox.cboxsls.nplusone.worker1.cor",ncorrupt)
+                       
     error_check(k1-k0==nfiles,'Expecting to have %d files more: see k1=%d k0=%d'%(nfiles,k1,k0))
 
-    fatal_check(ncorrupt==0, 'Corrupted files (%s) found'%ncorrupt)
+    fatal_check(ncorrupt==0, 'Corrupted files (%d) found'%ncorrupt) #Massimo 12-APR
+
 
 
 
