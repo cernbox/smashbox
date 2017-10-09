8aaa83c9edbfc56028a458decb2b876614e3b5a0
yolanda93 <yolanda93h@gmail.com>
2017-10-09 09:01:50 +0200

    added configurable parameter kibana_activity

--- python/smashbox/monitoring/kibana_monitoring.py
+++ python/smashbox/monitoring/kibana_monitoring.py
@@ -32,7 +32,7 @@ class StateMonitor:
                 print c, config[c]
 
         # initialize json to be sent for monitoring
-        self.test_results = {"activity": "smashbox-regression", 'test_name': testname, 'hostname': socket.gethostname(),
+        self.test_results = {"activity": config.kibana_activity, 'test_name': testname, 'hostname': socket.gethostname(),
                              'oc_client_version': str(str(ocsync_version())[1:-1].replace(",",".")),'oc_server': config.oc_server.split("/")[0],'platform': platform.system() + platform.release(),
                              'parameters':parameters,'parameters_text':str(parameters),'errors': [],'errors_text': "",'success': [],
                              'total_errors':0,'total_success':0, 'qos_metrics': [],'passed': 0,'failed': 0 }
