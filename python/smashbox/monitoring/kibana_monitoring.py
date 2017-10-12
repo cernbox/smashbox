8da2bd9812b46f9cf0e2ba0b5af0d8c8be6eed5b
yolanda93 <yolanda93h@gmail.com>
2017-10-12 11:46:28 +0200

    removed spaces from ocsync_version

--- python/smashbox/monitoring/kibana_monitoring.py
+++ python/smashbox/monitoring/kibana_monitoring.py
@@ -39,7 +39,7 @@ class StateMonitor:
 
         # initialize json to be sent for monitoring
         self.test_results = {"activity": config.kibana_activity, 'test_name': testname, 'hostname': socket.gethostname(),
-                             'oc_client_version': str(str(ocsync_version())[1:-1].replace(",",".")),'oc_server': config.oc_server.split("/")[0],'platform': client_platform,
+                             'oc_client_version': str(str(ocsync_version())[1:-1].replace(", ",".")),'oc_server': config.oc_server.split("/")[0],'platform': client_platform,
                              'parameters':parameters,'parameters_text':str(parameters),'errors': [],'errors_text': "",'success': [],
                              'total_errors':0,'total_success':0, 'qos_metrics': [],'passed': 0,'failed': 0 }
 
