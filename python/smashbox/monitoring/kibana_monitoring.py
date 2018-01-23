ad809a49e466fa569a8519bee78e238f6b68fd2b
yolanda93 <yolanda93h@gmail.com>
2018-01-23 13:58:33 +0100

    added counter ignoredFailures

--- python/smashbox/monitoring/kibana_monitoring.py
+++ python/smashbox/monitoring/kibana_monitoring.py
@@ -13,7 +13,7 @@ class StateMonitor:
         self.kibana_monitoring_host = config.get('kibana_monitoring_host', None)
         self.kibana_monitoring_port = config.get('kibana_monitoring_port', 10012)
 
-        expected_result = config.get('expected_result', {'Value':"BugFixed","Issue":""})
+        self.expected_result = config.get('expected_result', {'Value':"BugFixed","Issue":""})
 
         if not self.kibana_monitoring_host:
             self.worker_results = None
@@ -44,11 +44,12 @@ class StateMonitor:
         else:
             client_platform = platform.system() + platform.release()
 
+
         # initialize json to be sent for monitoring
         self.test_results = {"activity": config.kibana_activity, 'test_name': testname, 'hostname': socket.gethostname(),
                              'oc_client_version': str(str(ocsync_version())[1:-1].replace(", ",".")),'oc_server': config.oc_server.split("/")[0],'platform': client_platform,
                              'parameters':parameters,'parameters_text':str(parameters),'errors': [],'errors_text': "",'success': [],
-                             'total_errors':0,'total_success':0, 'qos_metrics': [],'passed': 0,'failed': 0, 'expected_result':expected_result }
+                             'total_errors':0,'total_success':0, 'qos_metrics': [],'ignoredFailures':0,'passed': 0,'failed': 0, 'expected_result':self.expected_result }
 
 
     def join_worker_results(self):
@@ -71,12 +72,15 @@ class StateMonitor:
         if not self.kibana_monitoring_host:
             return
 
-        if(self.test_results['total_errors']>=1): # A subtest is considered failed with one or more errors
-            self.test_results['passed'] = 0
-            self.test_results['failed'] = 1
+        if self.expected_result['Value'] == 'BugFixed': # we just want to track tests that we considered fixed
+            if(self.test_results['total_errors']>=1): # A subtest is considered failed with one or more errors
+                self.test_results['passed'] = 0
+                self.test_results['failed'] = 1
+            else:
+                self.test_results['failed'] = 0
+                self.test_results['passed'] = 1
         else:
-            self.test_results['failed'] = 0
-            self.test_results['passed'] = 1
+            self.test_results['ignoredFailures'] = 1 # if the test is ignored we put it in a separate counter
 
         json_results = self.get_json_results()
 
