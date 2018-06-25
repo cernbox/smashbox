125400155d67f56568361d6cdd36447274f71d69
root <root@giorgios1.dyndns.cern.ch>
2018-06-25 16:00:44 +0200

    add more testsets

--- lib/migration/lsync.py
+++ lib/migration/lsync.py
@@ -3,6 +3,11 @@
 # IMG_20171005_105517.jpg : NOT CHANGED
 # IMG_20171005_110734.jpg : NOT CHANGED
 # NEW.txt		  : NEW FILE
+# test.txt		  : MODIFY EXISTING FILE
+# torename.txt		  : RENAME FILE TO EXISTING FILE -> existing.txt
+
+# Photos/IMG_20180615_224839.jpg : MOVED -> FOLDERX
+# FOLDER_MOVED			 : MOVED -> FOLDERX
 
 
 from smashbox.utilities import *
@@ -11,6 +16,9 @@ import glob
 
 subdirPath = ""
 filesizeKB = 5000
+
+FOLDER_FILES = ['IMG_20180615_224839.jpg', 'IMG_20180615_224840.jpg', 'IMG_20180616_232842.jpg']
+
 
 def expect_content(fn,md5):
     actual_md5 = md5sum(fn)
@@ -68,13 +76,13 @@ def winner(step):
 
     list_files(subdir)
 
-    remove_file(os.path.join(subdir,'IMG_20171005_105458.jpg'))
-
-    createfile(os.path.join(subdir,'NEW.txt'),'1',count=1000,bs=filesizeKB)
-
-    shutil.move(os.path.join(subdir, 'IMG_20171005_105509.jpg'), os.path.join(subdir, 'IMG_20171005_105509-RENAMED.jpg'))
-
-    createfile(os.path.join(subdir,'test.txt'),'1',count=1000,bs=filesizeKB)
+    remove_file(os.path.join(subdir,'IMG_20171005_105458.jpg')) 	# IMG_20171005_105458.jpg : REMOVED
+    createfile(os.path.join(subdir,'NEW.txt'),'1',count=1000,bs=filesizeKB)	# NEW.txt : NEW FILE
+    shutil.move(os.path.join(subdir, 'IMG_20171005_105509.jpg'), os.path.join(subdir, 'IMG_20171005_105509-RENAMED.jpg')) 	# IMG_20171005_105509.jpg : RENAMED -> IMG_20171005_105509-RENAMED.jpg
+    createfile(os.path.join(subdir,'test.txt'),'1',count=1000,bs=filesizeKB)	# test.txt : MODIFY EXISTING FILE
+    shutil.move(os.path.join(subdir, 'torename.txt'), os.path.join(subdir, 'existing.txt'))
+    shutil.move(os.path.join(subdir, 'Photos/IMG_20180615_224839.jpg'), os.path.join(subdir, 'FOLDERX/IMG_20180615_224839.jpg'))	# Photos/IMG_20180615_224839.jpg : MOVED -> FOLDERX
+    mv(os.path.join(subdir, 'FOLDER_MOVED'), os.path.join(subdir, 'FOLDERX/FOLDER_MOVED'))	# FOLDER_MOVED : MOVED -> FOLDERX
 
 
 
@@ -89,6 +97,11 @@ def winner(step):
     shared['md5_test'] = md5sum(os.path.join(subdir,'test.txt'))
     logger.info('md5_test: %s',shared['md5_test'])
 
+    shared['md5_IMG_20180615_224839.jpg'] = md5sum(os.path.join(subdir,'FOLDERX/IMG_20180615_224839.jpg'))
+    logger.info('md5_IMG_20180615_224839.jpg: %s',shared['md5_IMG_20180615_224839.jpg'])
+
+    shared['md5_existing'] = md5sum(os.path.join(subdir,'existing.txt'))
+    logger.info('md5_existing: %s',shared['md5_existing'])
 
 
     run_ocsync(d)
@@ -132,51 +145,20 @@ def final_check(d,shared):
     expect_content(os.path.join(d,'NEW.txt'), shared['md5_NEW'])
     expect_content(os.path.join(d,'IMG_20171005_105509-RENAMED.jpg'), shared['md5_IMG_20171005_105509-RENAMED.jpg'])
     expect_content(os.path.join(d,'test.txt'), shared['md5_test'])
-    #expect_deleted_files(d, ['IMG_20171005_105458.jpg'])
+    expect_content(os.path.join(d,'existing.txt'), shared['md5_existing'])
+    expect_content(os.path.join(d,'FOLDERX/IMG_20180615_224839.jpg'), shared['md5_IMG_20180615_224839.jpg'])
 
-###############################################################################
+    d1 = os.path.join(d,'FOLDER_MOVED')
+    d2 = os.path.join(d, 'FOLDERX/FOLDER_MOVED')
+    logger.info('checking %s',d1)
+    error_check(not os.path.exists(d1), "path %s should not exist"%d1)
 
-def final_check_1_5(d): # this logic applies for 1.5.x client and owncloud server...
-    """ Final verification: all local sync folders should look the same. We expect conflicts and handling of deleted files depending on the rmLocalStateDB option. See code for details.
-    """
-    import glob
+    logger.info('checking %s',d2)
+    error_check(os.path.isdir(d2), "path %s should be a directory"%d2)
 
-    list_files(d)
-
-    conflict_files = glob.glob(os.path.join(d,'*_conflict-*-*'))
-
-    logger.debug('conflict files in %s: %s',d,conflict_files)
-
-    if not rmLocalStateDB:
-        # we expect exactly 1 conflict file
-
-        logger.warning("FIXME: currently winner gets a conflict file - exclude list should be updated and this assert modified for the winner")
-
-        error_check(len(conflict_files) == 1, "there should be exactly 1 conflict file (%d)"%len(conflict_files))
-    else:
-        # we expect exactly 3 conflict files
-        error_check(len(conflict_files) == 3, "there should be exactly 3 conflict files (%d)"%len(conflict_files))
-
-    for fn in conflict_files:
-
-        if not rmLocalStateDB:
-            error_check('_BOTH' in fn, """only files modified in BOTH workers have a conflict -  all other files should be conflict-free""")
-
-        else:
-            error_check('_BOTH' in fn or '_LOSER' in fn or '_WINNER' in fn, """files which are modified by ANY worker have a conflict now;  files which are not modified should not have a conflict""")
-
-    deleted_files = glob.glob(os.path.join(d,'*_DELETED*'))
-
-    logger.debug('deleted files in %s: %s',d,deleted_files)
-
-    if not rmLocalStateDB:
-        error_check(len(deleted_files) == 0, 'deleted files should not be there normally')
-    else:
-        # deleted files "reappear" if local sync db is lost on the loser, the only file that does not reappear is the DELETED_BOTH which was deleted on *all* local clients
-
-        error_check(len(deleted_files) == 2, "we expect exactly 2 deleted files")
-
-        for fn in deleted_files:
-            error_check('_LOSER' in fn or '_WINNER' in fn, "deleted files should only reappear if delete on only one client (but not on both at the same time) ")
+    for fn in FOLDER_FILES:
+        f = os.path.join(d2,fn)
+        logger.info("checking %s",f)
+        error_check(os.path.isfile(f), "path %s should be a file"%f)
 
 
