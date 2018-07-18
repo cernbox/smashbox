7f2d88b337c7645642c3789171b1e9e7123156e9
Jakub Moscicki <Jakub.Moscicki@cern.ch>
2018-07-18 17:02:40 +0200

    scan eos directly from priviledged host

--- lib/migration/scan_propfind.py
+++ lib/migration/scan_propfind.py
@@ -2,6 +2,8 @@ from smashbox.utilities import *
 from smashbox.utilities.hash_files import *
 from smashbox.protocol import *
 
+import smashbox.curl
+
 """
 Observations:
 
@@ -15,16 +17,36 @@ NOTE: I need to handle ETAG quotes in my script (set_tmp_etags.sh)!
 @add_worker
 def main(step):
 
-    d = make_workdir()
+    user=config.oc_account_name
+    user_letter=user[0]
+    server=config.oc_server
 
-    URL = oc_webdav_url()
+    assert(user!='.')
+    assert(config.oc_server in ['eoshome','eosuser'])
 
+    if config.oc_server == 'eoshome':
+       server = 'eoshome-'+user_letter
+
+    URL = 'http://'+server+':8000/cernbox/desktop/remote.php/webdav/home/'
+
+    smashbox.curl.Client.extra_headers={'Remote-user':user, 'CBOX-CLIENT-MAPPING':'cernbox/desktop/remote.php/webdav/home', 'CBOX-SERVER-MAPPING':'eos/user/%s/%s'%(user_letter,user),'X-Real-IP':'127.0.0.1'}
 
     propfind = ls_prop_desktop20
     #propfind = all_prop_android
 
     BASEURL=URL.replace('/cernbox/desktop/remote.php/webdav/home','')
 
+    NSDAV="{DAV:}"
+    NSOC="{http://owncloud.org/ns}"
+
+    FILTER_OUT_ATTRS=False
+    
+    ids_files=[]
+    ids_directories=[]
+    etags_directories=[]
+
+    fout = file('report.propfind.%s.%s.txt'%(user,config.oc_server),'w')
+    
     def scan_dir(URL):
         r0=propfind(URL,depth=0).propfind_response
         r1=propfind(URL,depth=1).propfind_response
@@ -32,10 +54,21 @@ def main(step):
         # the parent dir (.) reported by Depth1 must be identical as the parent dir reported by Depth0
         r10=[x for x in r1 if x[0] == r0[0][0]]
         assert r0 == r10
+        
+        path=r0[0][0]
+        attrs=r0[0][1]['HTTP/1.1 200 OK']
 
-        print r0[0][0],r0[0][1]['HTTP/1.1 200 OK']
+        ids_directories.append(attrs[NSOC+'id'])
+        etags_directories.append(attrs[NSDAV+'getetag'])
 
-        for x in r1:
+        if FILTER_OUT_ATTRS:
+            attrs[NSOC+'id'] = 'DDD' # this attribute does not matter for comparison of directories
+            attrs[NSDAV+'getlastmodified'] = 'DDD' # this attribute does not matter for comparison of directories
+            attrs[NSDAV+'getetag'] = 'DDD' # this attribute does not matter for comparison of directories
+
+        print >> fout, repr(path),repr(attrs)
+
+        for x in sorted(r1):
             if x[0] == r0[0][0]:
                 continue
             path=x[0]
@@ -45,7 +78,26 @@ def main(step):
                 # recurse into the directory
                 scan_dir(BASEURL+path)
             else:
-                print path,attrs
 
+                ids_files.append(attrs[NSOC+'id'])
+                if FILTER_OUT_ATTRS:
+                    attrs[NSOC+'id'] = 'FFF' # this attribute does not matter for comparison of files
+                print >> fout, repr(path),repr(attrs)
 
     scan_dir(URL)
+
+    fout1 = file('report.f-ids.%s.%s.txt'%(user,config.oc_server),'w')
+    for x in sorted(ids_files):
+        print >> fout1, x
+
+    fout2 = file('report.d-ids.%s.%s.txt'%(user,config.oc_server),'w')
+    for x in sorted(ids_directories):
+        print >> fout2, x
+
+    fout3 = file('report.d-etags.%s.%s.txt'%(user,config.oc_server),'w')
+    for x in sorted(etags_directories):
+        print >> fout3, x
+
+
+
+
