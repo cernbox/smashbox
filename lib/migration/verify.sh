951d6b73d35aa7197587b8f0331c6c0be2c3a699
Jakub Moscicki <jakub.moscicki@cern.ch>
2018-07-26 18:00:06 +0200

    all-in-one verification script

---
+++ lib/migration/verify.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+set -o errexit # bail out on all errors immediately
+
+function error() {
+    echo ERROR: $1
+    exit -1
+}
+
+ACCOUNT=$1
+
+if [ -z "$ACCOUNT" ]; then
+
+echo "Missing argument: account_name"
+exit -2
+
+fi
+
+mkdir -p /tmp/report
+
+cd /tmp/report
+
+/root/smashbox/bin/smash  -q /root/smashbox/lib/migration/scan_propfind.py -c /root/smashbox/lib/migration/scan.conf -o oc_account_name=$ACCOUNT -o oc_server=eoshome
+/root/smashbox/bin/smash  -q /root/smashbox/lib/migration/scan_propfind.py -c /root/smashbox/lib/migration/scan.conf -o oc_account_name=$ACCOUNT -o oc_server=eosuser
+
+# CHECK THE METADATA -- ALL SHOULD BE PROPERLY ALIGN (PLACEHODLER VALUES ARE EXCEPTION)
+
+diff report.propfind.${ACCOUNT}.eoshome.txt report.propfind.${ACCOUNT}.eosuser.txt || error "metadata mismatch"
+
+# CHECK FOR REPEATED METADATA VALUES -- SHOULD BE NONE
+
+X=`comm -12 report.d-etags.${ACCOUNT}.eosuser.txt  report.d-etags.${ACCOUNT}.eoshome.txt`
+
+if [ -n "$X" ]; then 
+echo $X
+error "Repeated directory etags found" 
+fi
+
+X=`comm -12 report.d-ids.${ACCOUNT}.eosuser.txt  report.d-ids.${ACCOUNT}.eoshome.txt`
+
+if [ -n "$X" ]; then 
+echo $X
+error "Repeated directory ids found" 
+fi
+
+X=`comm -12 report.f-ids.${ACCOUNT}.eosuser.txt  report.f-ids.${ACCOUNT}.eoshome.txt`
+
+if [ -n "$X" ]; then 
+echo $X
+error "Repeated file ids in found" 
+fi
+
+echo OK: ${ACCOUNT}
