079128ad0cbd4fe4fd2aa354183ff7ae5dffa09a
Anand Krishna <40204976+anand2312@users.noreply.github.com>
2022-07-29 15:04:44 +0530

    Reformat with black

--- python/smashbox/config.py
+++ python/smashbox/config.py
@@ -14,14 +14,17 @@ logger = None
 def get_logger(name: str = "config", level: int | None = None) -> logging.Logger:
     global logger
     if not logger:
-      if level is None:
-          level = logging.INFO  # change here to DEBUG if you want to debug config stuff
-      logging.basicConfig(level=level)
-    return logging.getLogger('.'.join(['smash', name]))
+        if level is None:
+            level = (
+                logging.INFO
+            )  # change here to DEBUG if you want to debug config stuff
+        logging.basicConfig(level=level)
+    return logging.getLogger(".".join(["smash", name]))
 
 
 class Configuration(BaseSettings):
     """Root configuration object that parses the values defined in the config file."""
+
     smashdir: str
     oc_account_name: str | None
     oc_number_test_users: int
@@ -41,7 +44,9 @@ class Configuration(BaseSettings):
     workdir_runid_enabled: bool
     oc_account_runid_enabled: bool
 
-    oc_account_reset_procedure: Literal["delete", "keep"]  # there are some more types that are not yet implemented
+    oc_account_reset_procedure: Literal[
+        "delete", "keep"
+    ]  # there are some more types that are not yet implemented
     rundir_reset_procedure: Literal["delete", "keep"]
 
     web_user: str
@@ -59,13 +64,13 @@ class Configuration(BaseSettings):
     @property
     def oc_server_datadirectory(self) -> str:
         return os.path.join("/var/www/html", self.oc_root, "data")
-    
+
     # these methods exists for backwards compatibility
     def _dict(self, **args: Any) -> dict[str, object]:
         """Returns a dictionary representation of the configuration object.
         Any extra arguments passed are also returned in this dictionary."""
         return {**self.dict(), **args}
-    
+
     def get(self, key: str, default: object) -> object:
         """Returns the value of the specified setting, or the
         default if the key doesn't exist."""
@@ -74,7 +79,9 @@ class Configuration(BaseSettings):
         return self._dict().get(key, default=default)
 
 
-def log_config(config: Configuration, level: int | None = None, hide_password: bool = False) -> None:
+def log_config(
+    config: Configuration, level: int | None = None, hide_password: bool = False
+) -> None:
     """Dump the entire configuration to the logging system at the given level.
     If hide_password=True then do not show the real value of the options which contain "password" in their names.
     """
@@ -90,8 +97,10 @@ def load_config(fp: Path | str) -> Configuration:
     with open(fp, "r") as file:
         return Configuration(**yaml.load(file))
 
+
 def configure_from_blob(fp: Path | str) -> Configuration:
     return pickle.load(fp)
+
 
 def dump_config(config: Configuration, fp: Path | str) -> None:
     """Serialize given config object as YAML and write it to the specified file."""
